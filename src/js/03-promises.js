f9d5082db5f0c7d7f370de9025a610209f95ffd9
Babenko Liudmyla <savelieva0509@gmail.com>
2022-10-01 17:42:02 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,48 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from "notiflix";
+
+const formEl = document.querySelector('.form');
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  let delay = Number(event.currentTarget.delay.value);
+  let step = Number(event.currentTarget.step.value);
+  let amount = Number(event.currentTarget.amount.value);
+  
+
+  for (let position = 1; position <= amount; position += 1) {
+
+ const onSuccess = ({ position, delay }) => {
+  setTimeout(() => {
+      Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  }, delay)
+   
+  }
+const onError = ({ position, delay }) => {
+  setTimeout(() => {
+    Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+  }, delay)
+}
+
+createPromise(position, delay)
+  .then(onSuccess)
+  .catch(onError)
+    
+  delay += step;  
   }
 }
+
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
+  return new Promise((resolve, reject) => {
+    if (shouldResolve) {
+      resolve({ position, delay });
+    } else {
+      reject({ position, delay });
+    }
+  }, delay)
+}
+
+
+
