88fc6932e1bda81c0aac7e0b71843e3d3e7ffaa8
Babenko Liudmyla <savelieva0509@gmail.com>
2022-10-02 13:28:40 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+let colorChangeIntervalId = null;
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+
+
+startBtn.addEventListener('click', onStartBtnClick);
+stopBtn.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick() {
+  document.body.style.backgroundColor = getRandomHexColor();
+  colorChangeIntervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+
+   startBtn.disabled = true;
+   stopBtn.disabled = false;
+   
+}
+
+function onStopBtnClick() {
+  clearInterval(colorChangeIntervalId);
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+}
